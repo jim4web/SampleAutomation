0bad19474ab137218bf01e4331b9c1deeb617c3f
jim4web <jimsha.pu@gmail.com>
2022-09-02 17:08:54 +0400

    First Sample commit

---
+++ SampleAutomation/src/test/java/test/TestFirstPgm.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.Test;
+
+public class TestFirstPgm {
+	
+	@Test
+	public void testdata() {
+		System.out.println("displaying through method");
+	}
+	
+/*
+	public static void main(String[] args) {
+	System.out.println("Welcome to my new world");
+	}
+*/
+}
---
+++ SampleAutomation/src/test/java/test/TestSecondPgm.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Test;
+
+public class TestSecondPgm {
+
+	@Test
+	public void display() {
+		System.out.println("My second class...");
+	}
+}
